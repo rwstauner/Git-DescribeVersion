e208a61201874bc49a169c2ff7e758c88a07a4de
Randy Stauner <randy@magnificent-tears.com>
2010-10-16 22:44:48 -0700

    Add simple module for executing in one line
    perl -MGit::DescribeVersion::App -e run

---
+++ lib/Git/DescribeVersion/App.pm
@@ -0,0 +1,23 @@
+package Git::DescribeVersion::App;
+# ABSTRACT: Provide a simple way to run Git::DescribeVersion as an app
+
+use strict;
+use warnings;
+use Git::DescribeVersion ();
+
+# simple: enable `perl -MGit::DescribeVersion::App -e run`
+sub import {
+	*main::run = \&run;
+}
+
+sub run {
+	my $opts = {};
+	foreach my $opt ( keys %Git::DescribeVersion::Defaults ){
+		my $eopt = "\UGIT_DV_$opt";
+		$opts->{$opt} = $ENV{$eopt} if exists($ENV{$eopt});
+	}
+
+	print Git::DescribeVersion->new('.', $opts)->version, "\n";
+}
+
+1;
