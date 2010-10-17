48c40bd48bd03ce60e76397430c3d104d619352c
Randy Stauner <randy@magnificent-tears.com>
2010-10-16 23:45:40 -0700

    Document usage and options with POD

--- lib/Git/DescribeVersion.pm
+++ lib/Git/DescribeVersion.pm
@@ -1,6 +1,17 @@
 package Git::DescribeVersion;
 # ABSTRACT: Use git-describe to determine a git repo's version
 
+=head1 SYNOPSIS
+
+	use Git::DescribeVersion ();
+	Git::DescribeVersion->new('.', {opt => 'value'});
+
+Or this one-liner:
+
+	perl -MGit::DescribeVersion::App -e run
+
+=cut
+
 use strict;
 use warnings;
 
@@ -66,3 +77,31 @@ sub version_from_count_objects {
 }
 
 1;
+
+=head1 OPTIONS
+
+These options can be passed to C<new()>:
+
+=head2 first_version
+
+If the repository has no tags at all, this version
+is used as the first version for the distribution.  It defaults to "v0.1".
+Then git objects will be counted and appended to create a version like "v0.1.5".
+
+=head2 version_regexp
+
+Regular expression that matches a tag containing
+a version.  It must capture the version into $1.  Defaults to C<^v([0-9._]+)$>
+which matches tags like C<"v0.1">.
+
+=head2 match_pattern
+
+A shell-glob-style pattern to match tags
+(default "v[0-9]*").  This is passed to C<git-describe> to help it
+find the right tag to count commits from.
+
+=head1 SEE ALSO
+
+=for :list
+* C<git>: L<http://www.git-scm.com|git>
+* L<Git::Wrapper>
