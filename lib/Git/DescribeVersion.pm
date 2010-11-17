e076fd705af92c70070665998a64a11bd26a6938
Randy Stauner <randy@magnificent-tears.com>
2010-11-17 11:41:04 -0700

    Fix typo in POD module link (insert missing 'Plugin')

--- lib/Git/DescribeVersion.pm
+++ lib/Git/DescribeVersion.pm
@@ -227,7 +227,7 @@ command line statement (so that I could put I<that> in my Makefiles).
 
 =for :list
 * L<Git::DescribeVersion::App>
-* L<Dist::Zilla::Git::DescribeVersion>
+* L<Dist::Zilla::Plugin::Git::DescribeVersion>
 * L<Git::Wrapper>
 * L<http://www.git-scm.com>
 * L<version>
